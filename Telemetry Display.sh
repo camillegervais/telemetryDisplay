ea95bc24178defca8c48a4d5f6a2faa24032b879
CAMILLE GERVAIS <camillegervais03@gmail.com>
2026-05-19 15:45:41 +0200

    fix bad start

--- Telemetry Display.sh
+++ Telemetry Display.sh
@@ -1,7 +1,7 @@
+"C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:5173/
 SCRIPT_PATH=${0%/*}
 if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then 
     cd $SCRIPT_PATH
 fi
 npm run dev
 ^C
-chrome http://localhost:5173/
