6fb544458a0c8fe7a6d196aca5ff98c0f8fff156
CAMILLE GERVAIS <camillegervais03@gmail.com>
2026-06-04 17:25:39 +0200

    config mapreplay.localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Telemetry Display API", version="0.1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
